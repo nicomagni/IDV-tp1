6a988c38b8d75e432458f4857da9068dadccd20c
guillermo liss <lissguillermo@gmail.com>
2012-08-17 22:22:18 -0300

    se agrega manager de asteroides provisorio

---
+++ Asterix/Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections;
+
+public class AsteroidController : MonoBehaviour {
+
+	float xRotation, yRotation, zRotation;
+	float xMove, zMove;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	void OnEnable() {
+		float cameraSize = (float) Camera.main.camera.orthographicSize + 1.0f;
+		float xPos, zPos;
+
+		float randX = (float) UnityEngine.Random.Range(1.0f, 2.0f);
+		float randZ = (float) UnityEngine.Random.Range(1.0f, 2.0f);
+		
+		// Choose a random spot to spawn, always on a camera border
+		float randomBorder = UnityEngine.Random.Range(0.0f, 1.0f);
+		if(randomBorder < 0.5f) {
+			xPos = (float) UnityEngine.Random.Range(-cameraSize, cameraSize);
+			zPos = randomBorder > 0.25f ? -cameraSize : cameraSize;
+			zMove = randomBorder > 0.25f ? randZ : -randZ;
+			xMove = UnityEngine.Random.Range(0.0f, 1.0f) > 0.5f ? randX : -randX;
+		} else {
+			zPos = (float) UnityEngine.Random.Range(-cameraSize, cameraSize);
+			xPos = randomBorder > 0.75f ? -cameraSize : cameraSize;
+			xMove = randomBorder > 0.75f ? randX : -randX;
+			zMove = UnityEngine.Random.Range(0.0f, 1.0f) > 0.5f ? randZ : -randZ;
+		}
+		
+		Debug.Log("cameraSize: " + cameraSize);
+		
+		this.transform.position = new Vector3(xPos, 0, zPos);
+				
+		// Choose random torque
+		float torqueLimit = 10.0f;
+		xRotation = (float) UnityEngine.Random.Range(-torqueLimit, torqueLimit);
+		yRotation = (float) UnityEngine.Random.Range(-torqueLimit, torqueLimit);
+		zRotation = (float) UnityEngine.Random.Range(-torqueLimit, torqueLimit);
+		
+		Debug.Log(xMove + " " + zMove);
+		
+//		float magnitude = 1000000000.0f;
+//		this.rigidbody.AddRelativeForce(new Vector3(xMove * magnitude, 0, zMove * magnitude));		
+//		transform.TransformDirection(new Vector3(xMove, 0, zMove));
+//		this.rigidbody.AddTorque(xRotation * magnitude, yRotation * magnitude, zRotation* magnitude);
+	}
+ 	
+	// Update is called once per frame
+	void Update () {
+		
+		// Deactivate the enemy bug if it reachs a camera border
+		float cameraSize = (float) Camera.main.camera.orthographicSize + 3.0f;
+		if(this.transform.position.z < -cameraSize || this.transform.position.z > cameraSize ||
+			this.transform.position.x < -cameraSize || this.transform.position.x > cameraSize) {
+			this.gameObject.active = false;
+		}
+		
+		this.transform.position = new Vector3(this.transform.position.x, 0, this.transform.position.z);
+		this.rigidbody.AddRelativeForce(new Vector3(xMove, 0, zMove));
+		this.rigidbody.AddTorque(xRotation, yRotation, zRotation);
+	}
+}
---
+++ Asterix/Assets/Scripts/AsteroidManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class AsteroidManager : MonoBehaviour {
+
+	GameObject[] enemyBugs;
+
+	static int enemyBugCache = 20;
+	static float randLimit = 95.0f;
+	
+	// Use this for initialization
+	void Start () {
+		enemyBugs = new GameObject[enemyBugCache];
+
+		for(int i = 0; i < enemyBugs.Length; i++){
+			enemyBugs[i] = (GameObject) Instantiate(Resources.Load("AsteroidPrefab"));
+			enemyBugs[i].active = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rand = UnityEngine.Random.Range(0.0f, 100.0f);
+		if(rand > randLimit) {
+			Debug.Log(rand);
+			
+	        for(int i = 0; i < enemyBugs.Length; i++){
+				if(enemyBugs[i].active == false) {
+					enemyBugs[i].active = true;
+					break;
+				}
+	        }
+
+		}	
+	}
+}
